7c4fec1565ac0227a44dcbd40e389e16b53a9bf1
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-11-22 22:29:10 -0600

    feat: define content for home page banner content.

--- app/about/page.js
+++ app/about/page.js
@@ -2,12 +2,90 @@
 
 import React from 'react';
 import { Grid, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Link from 'next/link';
+import Image from 'next/image';
+import ProfileImage from '../../public/ProfileImage.jpeg';
 
 export default function About() {
 	return (
-		<Grid container spacing={1}>
-			<Grid item xs={12}>
-				<Typography variant="h3">About</Typography>
+		<Grid container>
+			<Grid item={9} alignItems="center">
+				<Grid item xs={12}>
+					<Typography variant="homePageTitle">Hello I'm Riley</Typography>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography variant="homePageSubtitle">
+						I'm a software engineer based in Springfield, Missouri.
+					</Typography>
+				</Grid>
+				<Grid item xs={12} sx={{ display: 'flex', gap: '50px' }}>
+					<Grid item>
+						<Link 
+							href="https://github.com/rileygrotenhuis"
+							target="__blank"
+							style={{
+								color: 'inherit',
+								textDecoration: 'none',
+							}}
+						>
+							<GitHubIcon />
+						</Link>
+					</Grid>
+					<Grid item>
+						<Link 
+							href="https://www.linkedin.com/in/rileygrotenhuis"
+							target="__blank"
+							style={{
+								color: 'inherit',
+								textDecoration: 'none',
+							}}
+						>
+							<LinkedInIcon />
+						</Link>
+					</Grid>
+					<Grid item>
+						<Link 
+							href="https://twitter.com/ryleygrotenhuis"
+							target="__blank"
+							style={{
+								color: 'inherit',
+								textDecoration: 'none',
+							}}
+						>
+							<TwitterIcon />
+						</Link>
+					</Grid>
+					<Grid item>
+						<Link 
+							href="https://www.instagram.com/rileygrotenhuis"
+							target="__blank"
+							style={{
+								color: 'inherit',
+								textDecoration: 'none',
+							}}
+						>
+							<InstagramIcon />
+						</Link>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item={3}>
+				<Image
+					alt="ProfileImage"
+					src={ProfileImage}
+					placeholder="blur"
+					style={{
+						width: '350px',
+						height: '350px',
+						borderRadius: '100%',
+						boxShadow: '0 0 5px 2px #282a2d'
+					}}
+				>
+				</Image>
 			</Grid>
 		</Grid>
 	);
--- styles/theme.js
+++ styles/theme.js
@@ -3,7 +3,17 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
 	palette: {
 		mode: 'dark'
-	}
+	},
+	typography: {
+		homePageTitle: {
+			fontSize: '4rem',
+			fontWeight: 'bold',
+		},
+		homePageSubtitle: {
+			fontSize: '1.5rem',
+			fontWeight: 'normal',
+		}
+	},
 });
 
 export default theme;
