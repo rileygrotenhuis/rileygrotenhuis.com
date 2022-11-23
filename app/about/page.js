a9eddc6f650140e8d160958b3c37d30ed570028b
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-11-22 22:43:14 -0600

    feat: move content to home page file.

--- app/about/page.js
+++ app/about/page.js
@@ -12,87 +12,11 @@ import ProfileImage from '../../public/ProfileImage.jpeg';
 
 export default function About() {
 	return (
-		<Grid 
-			container 
-			spacing={6}
-			justifyContent="center" 
-			sx={{ 
-				marginTop: '125px' 
-			}}
-		>
-			<Grid item={9} sx={{ height: 'fit-content', margin: 'auto 0' }}>
-				<Grid item xs={12}>
-					<Typography variant="homePageTitle">Hello I'm Riley</Typography>
-				</Grid>
-				<Grid item xs={12}>
-					<Typography variant="homePageSubtitle">
-						I'm a software engineer based in Springfield, Missouri.
-					</Typography>
-				</Grid>
-				<Grid item xs={12} sx={{ display: 'flex', gap: '50px', marginTop: '25px' }}>
-					<Grid item>
-						<Link 
-							href="https://github.com/rileygrotenhuis"
-							target="__blank"
-							style={{
-								color: 'inherit',
-								textDecoration: 'none',
-							}}
-						>
-							<GitHubIcon />
-						</Link>
-					</Grid>
-					<Grid item>
-						<Link 
-							href="https://www.linkedin.com/in/rileygrotenhuis"
-							target="__blank"
-							style={{
-								color: 'inherit',
-								textDecoration: 'none',
-							}}
-						>
-							<LinkedInIcon />
-						</Link>
-					</Grid>
-					<Grid item>
-						<Link 
-							href="https://twitter.com/ryleygrotenhuis"
-							target="__blank"
-							style={{
-								color: 'inherit',
-								textDecoration: 'none',
-							}}
-						>
-							<TwitterIcon />
-						</Link>
-					</Grid>
-					<Grid item>
-						<Link 
-							href="https://www.instagram.com/rileygrotenhuis"
-							target="__blank"
-							style={{
-								color: 'inherit',
-								textDecoration: 'none',
-							}}
-						>
-							<InstagramIcon />
-						</Link>
-					</Grid>
-				</Grid>
-			</Grid>
-			<Grid item={3}>
-				<Image
-					alt="ProfileImage"
-					src={ProfileImage}
-					placeholder="blur"
-					style={{
-						width: '350px',
-						height: '350px',
-						borderRadius: '100%',
-						boxShadow: '0 0 5px 2px #282a2d'
-					}}
-				>
-				</Image>
+		<Grid container spacing={1}>
+			<Grid item xs={12}>
+				<Typography variant="h3">
+					About Me
+				</Typography>
 			</Grid>
 		</Grid>
 	);
