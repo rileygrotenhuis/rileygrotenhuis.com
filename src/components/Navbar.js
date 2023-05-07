fc29efc9ba4bf6379b430de7cf9fae40263e0c5f
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-06 20:17:18 -0500

    fix: sizing of logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <nav>
             <div class="flex md:hidden justify-between py-8 px-12">
-                <h1 class="text-primary text-2xl border-2 rounded-[100%] p-4 cursor-pointer">RG</h1>
+                <h1 class="text-primary text-2xl border-2 rounded-[100%] p-3 cursor-pointer">RG</h1>
                 <div class={`absolute right-0 top-0 w-4/6 h-full bg-background-secondary ${!isMenuOpen ? 'hidden': ''}`}>
                     <button 
                         class="absolute top-12 right-12 flex flex-col items-center justify-center w-8 h-8 rounded-md"
@@ -44,7 +44,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div class="hidden md:flex justify-between py-8 px-12">
-                <h1 class="text-primary text-2xl border-2 rounded-[100%] p-4 cursor-pointer">RG</h1>
+                <h1 class="text-primary text-2xl border-2 rounded-[100%] p-3 cursor-pointer">RG</h1>
                 <ul class="text-text-primary font-extralight flex gap-16 my-auto text-sm">
                     <li class="hover:text-primary hover:cursor-pointer my-auto">
                         <span class="text-primary">01. </span>
