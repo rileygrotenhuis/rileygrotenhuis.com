13146d662c841a12f6519ce8ae15a5e861c1c1e5
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-07 00:53:39 -0500

    fix: styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,14 @@ export default function Contact() {
       id="contact"
       className="w-3/4 lg:w-[550px] text-center mx-auto mt-72 font-extralight"
     >
-      <p className="text-primary text-lg">03. What's Next</p>
+      <p className="text-primary text-lg">03. What Next?</p>
       <h3 className="text-5xl text-text-primary mt-4 font-bold">
         Get In Touch
       </h3>
       <p className="text-text-secondary mt-4">
-        Although I'm not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+        Although I am not currently looking for any new opportunities, my inbox
+        is always open. Whether you have a question or just want to say hi, I
+        will try my best to get back to you!
       </p>
       <a href="mailto: abc@example.com">
         <button className="py-4 px-8 mt-8 rounded-md border border-primary text-lg font-extralight text-primary">
