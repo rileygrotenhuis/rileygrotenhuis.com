19962484d5f33b515e7988ca8ac27459f9d99e0e
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-06 20:35:03 -0500

    feat: finish hero section

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="w-5/6 md:w-3/4 mx-auto">
+    <div className="w-5/6 md:w-4/6 mx-auto">
       <Hero />
     </div>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,17 @@
 export default function Hero() {
     return (
-        <div className="border">
+        <div className="mt-24">
             <span className="text-primary">Hi, my name is</span>
-            <h1 className="text-text-primary text-6xl font-bold">Riley Grotenhuis.</h1>
-            <h2 className="text-text-secondary text-6xl font-bold">I build things for the web.</h2>
-            <p className="text-text-secondary font-extralight w-2/4">
-                I’m a software engineer specializing in building 
-                exceptional digital experiences. Currently, I’m 
-                focused on building accessible, human-centered 
-                products at Upstatement.
+            <h1 className="text-text-primary text-5xl md:text-6xl font-bold mt-4">Riley Grotenhuis.</h1>
+            <h2 className="text-text-secondary text-5xl md:text-6xl font-bold mt-4">I build things for the web.</h2>
+            <p className="text-text-secondary font-extralight w-5/6 md:w-2/4 mt-8">
+                I'm a software engineer at
+                <a className="text-primary hover:underline" href="https://www.midwesterninteractive.com" target="__blank"> Midwestern Interactive </a>
+                working towards our mission of serving each other,
+                and serving our clients.
             </p>
             <a href="https://github.com/rileygrotenhuis" target="__blank">
-                <button className="border border-primary px-8 py-2 text-primary font-light rounded-md">
+                <button className="border border-primary px-8 py-2 text-primary font-light rounded-md mt-8">
                     Check out my GitHub
                 </button>
             </a>
