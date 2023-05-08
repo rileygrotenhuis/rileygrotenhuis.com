219d015d61e9cdb8f2c18967186fb7abab06e7e4
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-08 01:47:29 -0500

    feat: use more components

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import SecondaryNavbar from '@/components/navigation/SecondaryNavbar';
-import Link from 'next/link';
-import Image from 'next/image';
+import BlogTable from '@/components/blog/BlogTable';
 
 const getPostMetaData = () => {
   const folder = 'posts/';
@@ -29,26 +28,7 @@ export default function Blog() {
   return (
     <>
       <SecondaryNavbar />
-      <div className="w-5/6 md:w-[1000px] mx-auto mt-12">
-        <div className="flex flex-wrap gap-8">
-          {posts.map((item, index) => {
-            return (
-              <Link
-                key={index}
-                href={`/blog/${item.slug}`}
-                className="bg-background-secondary hover:shadow-xl transform hover:-translate-y-1 duration-300 hover:cursor-pointer text-text-secondary font-extralight px-4 py-8 rounded-lg w-72 flex flex-col gap-4 mx-auto"
-              >
-                <Image src="/images/blog.svg" width={48} height={48} />
-                <h3 className="text-text-primary hover:text-primary hover:cursor-pointer text-2xl font-normal">
-                  {item.title}
-                </h3>
-                <p className="text-lg">{item.date}</p>
-                <p>{item.subtitle}</p>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+      <BlogTable posts={posts} />
     </>
   );
 }
---
+++ src/components/blog/BlogTable.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function BlogTable(props) {
+  return (
+    <div className="w-5/6 md:w-[1000px] mx-auto mt-12">
+      <div className="flex flex-wrap gap-8">
+        {props.posts.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              href={`/blog/${item.slug}`}
+              className="bg-background-secondary hover:shadow-xl transform hover:-translate-y-1 duration-300 hover:cursor-pointer text-text-secondary font-extralight px-4 py-8 rounded-lg w-72 flex flex-col gap-4 mx-auto"
+            >
+              <Image src="/images/blog.svg" width={48} height={48} />
+              <h3 className="text-text-primary hover:text-primary hover:cursor-pointer text-2xl font-normal">
+                {item.title}
+              </h3>
+              <p className="text-lg text-primary">{item.date}</p>
+              <p>{item.subtitle}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
