39c21f62fa03283101a7caf378b719e5e858d0e8
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-08 00:45:39 -0500

    fix: slightly improve SEO

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -30,7 +30,7 @@ export default function BlogPost(props) {
     <>
       <BlogNavbar />
       <div className="w-5/6 md:w-1/2 mx-auto mt-84 md:mt-12">
-        <h3 className="text-text-primary text-4xl font-bold">{meta.title}</h3>
+        <h1 className="text-text-primary text-4xl font-bold">{meta.title}</h1>
         <p className="text-primary font-extralight mt-2">{meta.date}</p>
         <article className="text-text-secondary font-light">
           <Markdown className="markdown">{post.content}</Markdown>
