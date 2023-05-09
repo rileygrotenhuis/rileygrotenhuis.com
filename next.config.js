e0b451f4c93accd3d516febdaca0ae4c9a2c8c0d
Riley Grotenhuis <rg@buildmidwestern.com>
2023-05-08 19:07:32 -0500

    Remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.modules.push(__dirname + '/posts');
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
