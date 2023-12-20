9a24c69a06ad6294fa7da80cbc93efe3cc72484a
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 17:26:09 -0600

    feat: add basic hero section

---
+++ components/HeroSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <span class="text-primary">Hello, my name is</span>
+    <h1 class="text-6xl text-white font-bold mt-4 mb-4">Riley Grotenhuis.</h1>
+    <h3 class="text-6xl text-slate-400 font-bold mb-8">
+      I build things for the web.
+    </h3>
+    <p class="text-white font-extralight w-5/6 md:w-2/4">
+      I am a software engineer at Midwestern Interactive working towards our
+      mission of serving each other, and serving our clients.
+    </p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1 class="text-2xl text-primary">Home Page</h1>
+  <LazyHeroSection
+    class="w-5/6 md:w-4/6 mx-auto mt-8 md:mt-24 max-w-[1100px]"
+  />
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Riley Grotenhuis',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Riley Grotenhuis is a software engineer based out of the United States.',
+    },
+  ],
+});
+</script>
