35c5fd84eb1d16130c138daee312a1188a0599c5
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 17:34:57 -0600

    feat: add icons to hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -2,12 +2,22 @@
   <div>
     <span class="text-primary">Hello, my name is</span>
     <h1 class="text-6xl text-white font-bold mt-4 mb-4">Riley Grotenhuis.</h1>
-    <h3 class="text-6xl text-slate-400 font-bold mb-8">
+    <h3 class="text-6xl text-slate-400 font-bold">
       I build things for the web.
     </h3>
-    <p class="text-white font-extralight w-5/6 md:w-2/4">
-      I am a software engineer at Midwestern Interactive working towards our
-      mission of serving each other, and serving our clients.
+    <p class="text-white font-extralight w-5/6 md:w-2/4 mt-8 mb-2">
+      I am a software engineer at
+      <a
+        class="text-primary font-normal hover:underline"
+        href="https://www.buildmidwestern.com/"
+        >Midwestern Interactive
+      </a>
+      working towards our mission of serving each other, and serving our
+      clients.
     </p>
+    <div class="flex gap-2 items-center">
+      <IconsGitHubIcon />
+      <IconsLinkedInIcon />
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <LazyHeroSection
-    class="w-5/6 md:w-4/6 mx-auto mt-8 md:mt-24 max-w-[1100px]"
-  />
+  <HeroSection class="w-5/6 md:w-4/6 mx-auto mt-8 md:mt-24 max-w-[1100px]" />
 </template>
 
 <script setup lang="ts">
