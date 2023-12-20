ad72625c4e845c02006771b5d82a8def5eb8d594
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 17:42:38 -0600

    feat: add responsive nature

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <span class="text-primary">Hello, my name is</span>
-    <h1 class="text-6xl text-white font-bold mt-4 mb-4">Riley Grotenhuis.</h1>
-    <h3 class="text-6xl text-slate-400 font-bold">
+    <h1 class="text-5xl md:text-6xl text-white font-bold mt-4 mb-4">
+      Riley Grotenhuis.
+    </h1>
+    <h3 class="text-5xl md:text-6xl text-slate-400 font-bold">
       I build things for the web.
     </h3>
-    <p class="text-white font-extralight w-5/6 md:w-2/4 mt-8 mb-2">
+    <p class="text-white font-extralight w-5/6 md:w-2/4 mt-4 mb-2">
       I am a software engineer at
       <a
         class="text-primary font-normal hover:underline"
