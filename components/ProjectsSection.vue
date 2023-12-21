298bcc803b91c2efa7b91001730e7ac88a227591
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 19:34:49 -0600

    feat: add descriptions to all projects

--- components/ProjectsSection.vue
+++ components/ProjectsSection.vue
@@ -5,12 +5,16 @@ const projects = [
     subtitle: 'Segmented budgeting tool built using Laravel and Nuxt',
     url: 'https://www.corvesive.com',
     technologies: ['Laravel', 'Nuxt', 'MySQL'],
+    description:
+      'Corvesive is a budgeting application that focuses on tracking your personal finances over the course of customizable periods of times. This project has seen multiple iterations all trying to solve some of my own personal financial goals and needs, but this was the first time I have built it with the intention of releasing it to the public. You can signup for a free account and start trakcing your finances today!',
   },
   {
     title: 'rbranch',
     subtitle: 'Custom CLI tool for Git branch operations',
     url: 'https://github.com/rileygrotenhuis/rbranch',
     technologies: ['Go', 'Bubbletea'],
+    description:
+      'On certain projects that I have been apart of in the past, I have found myself typing out annoying Git branch names with specific issue tracking numbers and other prefixes. Instead of taking the 5 minutes to search a pre-existing solution for this problem (or even using a Git GUI), I decided to build it myself. I also slightly am using this as an on-going excuse to learn Go.',
   },
 ];
 </script>
