37f89c2933ca0c392b2df159aef6d1fae139fbe1
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 19:27:54 -0600

    chore: write basic project descriptions

---
+++ components/ProjectsSection.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const projects = [
+  {
+    title: 'Corvesive',
+    subtitle: 'Segmented budgeting tool built using Laravel and Nuxt',
+    url: 'https://www.corvesive.com',
+    technologies: ['Laravel', 'Nuxt', 'MySQL'],
+  },
+  {
+    title: 'rbranch',
+    subtitle: 'Custom CLI tool for Git branch operations',
+    url: 'https://github.com/rileygrotenhuis/rbranch',
+    technologies: ['Go', 'Bubbletea'],
+  },
+];
+</script>
