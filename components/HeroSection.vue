7fda4dc5c986d8d037ee489ea65689c831a0e65d
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-21 14:08:18 -0600

    feat: add random slogan animation slide in

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -4,8 +4,10 @@
     <h1 class="text-5xl md:text-6xl text-white font-bold mt-4 mb-4">
       Riley Grotenhuis.
     </h1>
-    <h3 class="text-5xl md:text-6xl text-slate-400 font-bold">
-      Professional code maestro.
+    <h3
+      class="text-5xl md:text-6xl text-slate-400 font-bold opacity-100 animate-fade-in"
+    >
+      {{ slogan }}
     </h3>
     <p class="text-white font-extralight w-5/6 md:w-2/4 mt-4 mb-2">
       Software engineer at
@@ -26,3 +28,29 @@
     </div>
   </div>
 </template>
+
+<style>
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.animate-fade-in {
+  animation: fadeIn 1s ease-in;
+}
+</style>
+
+<script setup lang="ts">
+const slogans = [
+  'Professional code maestro.',
+  'Professional code monkey.',
+  'I build things for the web.',
+  'I write code and other stuff.',
+];
+
+const slogan = slogans[Math.floor(Math.random() * slogans.length)];
+</script>
