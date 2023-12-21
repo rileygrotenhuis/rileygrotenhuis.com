790127d24814ebdb8217376afd9f0ef6dc2f6bef
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-20 18:42:18 -0600

    feat: create about section

---
+++ components/AboutSection.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="my-auto flex flex-col gap-8">
+      <h3 class="text-text-primary text-3xl">
+        <span class="text-primary text-xl">01. </span>
+        About Me
+      </h3>
+      <div class="flex flex-col gap-4">
+        <p class="leading-6">
+          Hi! My name is Riley and I love all things web development. My
+          interest in programming started back in 2015 when I decided to take my
+          first computer science course in high school. Since then I have done
+          extensive self learning and spent some time at Missouri State
+          University studying computer science.
+        </p>
+        <p class="leading-6">
+          I have had the privilege of working at
+          <a
+            class="text-primary hover:underline"
+            href="https://www.missouristate.edu/Web/default.htm"
+            target="__blank"
+          >
+            Missouri State University
+          </a>
+          as a student web developer where I worked on a lot of their internal
+          tooling as well as various web applications. Fast forward to today, I
+          have put a halt on my formal education and my main focus today is
+          building full stack applications at
+          <a
+            class="text-primary hover:underline"
+            href="https://www.buildmidwestern.com"
+            target="__blank"
+          >
+            Midwestern Interactive
+          </a>
+          for our various amounts of clients.
+        </p>
+        <p class="text-text-secondary leading-6">
+          You can learn a little bit more about me and my journey as a developer
+          here in this
+          <a
+            class="text-primary hover:underline"
+            href="/blog/my-career-so-far"
+            target="__blank"
+          >
+            blog post
+          </a>
+          !
+        </p>
+        <p class="leading-6">
+          Here are a few technologies I have been working with recently:
+        </p>
+        <div class="flex gap-32 ml-4">
+          <ul class="list-disc text-primary">
+            <li>
+              <span>TypeScript</span>
+            </li>
+            <li>
+              <span>Javascript</span>
+            </li>
+            <li>
+              <span>Php</span>
+            </li>
+            <li>
+              <span>Go</span>
+            </li>
+          </ul>
+          <ul class="list-disc text-primary">
+            <li>
+              <span>Laravel</span>
+            </li>
+            <li>
+              <span>Nuxt</span>
+            </li>
+            <li>
+              <span>Next (React)</span>
+            </li>
+            <li>
+              <span>TailwindCSS</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="m-auto">
+      <img
+        class="w-[1500px] rounded-md"
+        src="/profile.jpeg"
+        alt="Profile_Image"
+      />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <HeroSection class="w-5/6 md:w-4/6 mx-auto mt-8 md:mt-24 max-w-[1100px]" />
+  <AboutSection
+    id="about"
+    class="w-3/4 lg:w-7/12 mx-auto mt-72 flex flex-col md:flex-row gap-16 xl:mt-96 max-w-[1000px]"
+  />
 </template>
 
 <script setup lang="ts">
