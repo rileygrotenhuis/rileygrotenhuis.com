889626364ebc9dceaa5ec703247070e8c67f9733
Riley Grotenhuis <rg@buildmidwestern.com>
2023-12-21 13:37:09 -0600

    content

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -5,17 +5,16 @@
       Riley Grotenhuis.
     </h1>
     <h3 class="text-5xl md:text-6xl text-slate-400 font-bold">
-      I build things for the web.
+      Professional code maestro.
     </h3>
     <p class="text-white font-extralight w-5/6 md:w-2/4 mt-4 mb-2">
-      I am a software engineer at
+      Software engineer at
       <a
         class="text-primary font-normal hover:underline"
         href="https://www.buildmidwestern.com/"
         >Midwestern Interactive
       </a>
-      working towards our mission of serving each other, and serving our
-      clients.
+      based in Springfield, Missouri.
     </p>
     <div class="flex gap-2 items-center">
       <a href="https://github.com/rileygrotenhuis" target="__blank">
