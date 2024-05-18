1ae76c92de7a6e6bc3ee6b9e5b2af51dabfbf15e
Riley Grotenhuis <rg@buildmidwestern.com>
2024-05-18 03:58:16 -0500

    projects banners

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -2,11 +2,13 @@
 import MainLayout from '@/Layouts/MainLayout.vue';
 import Landing from '@/Pages/Home/Partials/Landing.vue';
 import CorvesiveBanner from '@/Pages/Home/Partials/CorvesiveBanner.vue';
+import ProjectsBanner from '@/Pages/Home/Partials/ProjectsBanner.vue';
 </script>
 
 <template>
   <MainLayout>
     <Landing />
     <CorvesiveBanner />
+    <ProjectsBanner />
   </MainLayout>
 </template>
---
+++ resources/js/Pages/Home/Partials/ProjectsBanner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full bg-primary-300">
+    <div
+      class="max-w-4xl mx-auto px-4 py-8 flex flex-wrap-reverse justify-center items-center gap-8"
+    >
+      <div>
+        <img
+          src="/images/projects.svg"
+          alt="Projects Banner"
+          class="w-[325px]"
+        />
+      </div>
+
+      <div>
+        <h3 class="text-primary-950 text-2xl font-bold mb-2">
+          Some of my other projects.
+        </h3>
+        <p class="text-primary-950 text-base mb-4 max-w-[400px]">
+          In tandem with Corvesive, I enjoy building developer tools and other
+          side projects as I find the time.
+        </p>
+        <a
+          href="https://github.com/rileygrotenhuis"
+          target="_blank"
+          class="text-white bg-primary-700 px-4 py-2 rounded-md mt-4 font-bold hover:bg-primary-500"
+        >
+          My Projects
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
