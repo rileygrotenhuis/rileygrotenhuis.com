7cefb803840fb7fc085057655cd4faeb5ad8e179
Riley Grotenhuis <rg@buildmidwestern.com>
2024-05-20 21:00:48 -0500

    contact section

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -4,6 +4,7 @@ import Landing from '@/Pages/Home/Partials/Landing.vue';
 import CorvesiveBanner from '@/Pages/Home/Partials/CorvesiveBanner.vue';
 import ProjectsBanner from '@/Pages/Home/Partials/ProjectsBanner.vue';
 import AboutSection from '@/Pages/Home/Partials/AboutSection.vue';
+import ContactSection from '@/Pages/Home/Partials/ContactSection.vue';
 </script>
 
 <template>
@@ -12,5 +13,6 @@ import AboutSection from '@/Pages/Home/Partials/AboutSection.vue';
     <AboutSection />
     <CorvesiveBanner />
     <ProjectsBanner />
+    <ContactSection />
   </MainLayout>
 </template>
---
+++ resources/js/Pages/Home/Partials/ContactSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SendContactMessage from '@/Pages/Home/Partials/SendContactMessage.vue';
+</script>
+
+<template>
+  <div class="max-w-4xl mx-auto px-4 py-24">
+    <h3 class="text-white font-semibold text-3xl text-center mb-2">
+      Get connected
+    </h3>
+    <p class="text-white text-center max-w-[675px] mx-auto mb-4">
+      Although I am not currently looking for any new opportunities, my inbox is
+      always open. Whether you have a question or just want to say hi, I will
+      try my best to get back to you!
+    </p>
+    <div class="w-fit mx-auto">
+      <SendContactMessage />
+    </div>
+  </div>
+</template>
